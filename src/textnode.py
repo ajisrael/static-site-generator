1a1a84bd705b3e1b65ba451a46eb904463964877
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-04-15 13:10:09 -0400

    f   - added text to html node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
-    NORMAL = 'normal'
+    TEXT = 'text'
     BOLD = 'bold'
     ITALIC = 'italic'
     CODE = 'code'
@@ -10,7 +12,7 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type: TextType, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -20,3 +22,21 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type: 
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode('b', text_node.text)
+        case TextType.ITALIC:
+            return LeafNode('i', text_node.text)
+        case TextType.CODE:
+            return LeafNode('code', text_node.text)
+        case TextType.LINK:
+            return LeafNode('a', text_node.text, {'href': text_node.url})
+        case TextType.IMAGE:
+            return LeafNode('img', "", {'src': text_node.url, 'alt': text_node.text})
+        case _:
+            raise ValueError(f"invalid text type: {text_node.text_type}")    
