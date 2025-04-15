e2627ab636983672c1ca31caf54730d5aafcf754
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-04-15 12:46:21 -0400

    f   - created parent node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,3 +37,26 @@ class LeafNode(HTMLNode):
 
     def __repr__(self) -> str:
         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError('All parent nodes must have a tag')
+
+        if self.children == None:
+            raise ValueError('All parent nodes must have children')
+
+        html_str = f"<{self.tag}{self.props_to_html()}>"
+
+        for child in self.children:
+            html_str += child.to_html()
+
+        html_str += f"</{self.tag}>"
+
+        return html_str
+
+    def __repr__(self) -> str:
+        return f"ParentNode(tag={self.tag}, children={self.children}, props={self.props})"
