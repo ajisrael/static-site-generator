4ec41d3e7ecfa9a436975ec45bcce5e66175f2af
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-04-15 00:46:40 -0400

    t   - added unit test for text node class

--- src/textnode.py
+++ src/textnode.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class TextType(Enum):
     NORMAL = 'normal'
+    BOLD = 'bold'
     ITALIC = 'italic'
     CODE = 'code'
     LINK = 'link'
@@ -9,7 +10,7 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
