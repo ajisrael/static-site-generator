864428295c8974b7b0abc4ecb3a2567556ca0eeb
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-04-15 12:05:42 -0400

    f   - created leaf node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,19 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError('All leaf nodes must have a value')
+
+        if self.tag == None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
