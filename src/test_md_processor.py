1adab61bed5ea5b70e740b24953a986c9733ba0c
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-04-16 11:34:50 -0400

    t   - added unit test for text to textnodes function

--- src/test_md_processor.py
+++ src/test_md_processor.py
@@ -1,6 +1,6 @@
 import unittest
 
-from md_processor import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link
+from md_processor import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 
@@ -226,3 +226,28 @@ class TestMdProcessorSplitLinks(unittest.TestCase):
             TextNode("second link", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
         ]
         self.assertListEqual(new_nodes, expected_new_nodes)
+
+class TestMdProcessorTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        textnodes = text_to_textnodes(text)
+        expected_textnodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertListEqual(textnodes, expected_textnodes)
+
+    def test_text_to_textnodes_empty_text(self):
+        text = ""
+        textnodes = text_to_textnodes(text)
+        expected_textnodes = []
+        self.assertListEqual(textnodes, expected_textnodes)
+
